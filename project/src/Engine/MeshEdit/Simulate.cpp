a5c6d3a82fe98c59c8fd98cf1acd39a2b1a738d9
dqt <97156095+nanshoudedqt@users.noreply.github.com>
2023-05-30 11:03:49 +0800

    add a new method to simulate (Newton-Raphson Solver)

--- project/src/Engine/MeshEdit/Simulate.cpp
+++ project/src/Engine/MeshEdit/Simulate.cpp
@@ -44,7 +44,7 @@ bool Simulate::Init() {
 		unsigned large = edgelist[2 * i] < edgelist[2 * i + 1] ? edgelist[2 * i + 1] : edgelist[2 * i];
 
 		adj_map[make_pair(small, large)] = (positions[edgelist[2 * i]] - positions[edgelist[2 * i + 1]]).norm();
-		
+
 		if (log_verbosity > 50) {
 			printf("added edge (%d, %d), len=%lf\n", edgelist[2 * i], edgelist[2 * i + 1]
 				, getOrigLen(edgelist[2 * i], edgelist[2 * i + 1]));
@@ -57,10 +57,10 @@ bool Simulate::Init() {
 }
 
 bool Simulate::Run() {
-	SimulateOnce();
+	SimulateOnceNR();
+	//SimulateOnceMin();
 	curr_time += h;
 	updateExtForce();
-	// half-edge structure -> triangle mesh
 
 	return true;
 }
@@ -68,18 +68,14 @@ bool Simulate::Run() {
 void Ubpa::Simulate::updateKMat() {
 
 	size_t total_points = positions.size();
-	//size_t fixed_points = fixed_id.size();
-	size_t free_points = total_points;
 
 	// build K_mat
-	this->K_mat = SparseMatrix<float>(3 * free_points, 3 * total_points);
+	this->K_mat = SparseMatrix<float>(3 * total_points, 3 * total_points);
 	this->K_mat.setZero();
-	this->xf_to_x.clear();
 
 	int current_row = 0;
 	for (size_t i = 0; i < total_points; i++) {
 		K_mat.insert(current_row, i) = 1;
-		xf_to_x[current_row] = i;
 		current_row++;
 	}
 
@@ -103,7 +99,7 @@ float Simulate::getK(int i, int j) {
 
 // i, j are indices in total_points
 // with respect to x_i
-Eigen::Matrix3f Simulate::calcNablaFintComponent(int i, int j, const std::vector<pointf3> &pos_step_k) {
+Eigen::Matrix3f Simulate::calcNablaFintComponent(int i, int j, const std::vector<pointf3>& pos_step_k) {
 	Vector3f x_i = Vector3f{ pos_step_k[i][0], pos_step_k[i][1], pos_step_k[i][2] };
 	Vector3f x_j = Vector3f{ pos_step_k[j][0], pos_step_k[j][1], pos_step_k[j][2] };
 
@@ -111,9 +107,9 @@ Eigen::Matrix3f Simulate::calcNablaFintComponent(int i, int j, const std::vector
 	float l = getOrigLen(i, j);
 	float k = getK(i, j);
 
-	Matrix3f f_int_component = k * (( l / dist_norm) - 1) * Matrix3f::Identity()
+	Matrix3f f_int_component = k * ((l / dist_norm) - 1) * Matrix3f::Identity()
 		- k * l * (1.0 / powf(dist_norm, 3)) * (x_i - x_j) * (x_i - x_j).transpose();
-	
+
 	return f_int_component;
 }
 
@@ -144,7 +140,7 @@ std::vector<unsigned> Simulate::getAdjV(int vi) {
 }
 
 // K_block * ( g_mid )
-float Simulate::calcResidual(const std::vector<vecf3> &f_int, const Eigen::VectorXf &x_new, const std::vector<pointf3> &pos_step_k) {
+float Simulate::calcResidual(const std::vector<vecf3>& f_int, const Eigen::VectorXf& x_new, const std::vector<pointf3>& pos_step_k) {
 	// calculate ||g(x) - 0||^2
 	size_t n = positions.size();
 
@@ -153,7 +149,7 @@ float Simulate::calcResidual(const std::vector<vecf3> &f_int, const Eigen::Vecto
 	for (size_t i = 0; i < n; i++) {
 		Vector3f x_n = { this->positions[i][0], this->positions[i][1], this->positions[i][2] };
 		Vector3f v_n = { this->velocity[i][0], this->velocity[i][1], this->velocity[i][2] };
-		
+
 		Matrix3f m = Matrix3f::Identity() * mass[i];
 
 		Vector3f y = calcY(i);
@@ -164,7 +160,7 @@ float Simulate::calcResidual(const std::vector<vecf3> &f_int, const Eigen::Vecto
 			fint_total += calcFint(i, adjv, pos_step_k);
 		}
 
-		Vector3f g_mid = m * (Vector3f{ x_new[3 * i], x_new[3 * i + 1], x_new[3 * i + 2] } -y) - h * h * fint_total;
+		Vector3f g_mid = m * (Vector3f{ x_new[3 * i], x_new[3 * i + 1], x_new[3 * i + 2] } - y) - h * h * fint_total;
 
 		aggregated(3 * i) = g_mid(0);
 		aggregated(3 * i + 1) = g_mid(1);
@@ -177,9 +173,9 @@ float Simulate::calcResidual(const std::vector<vecf3> &f_int, const Eigen::Vecto
 
 
 
-void Simulate::SimulateOnce() {
+void Simulate::SimulateOnceMin() {
 
-	
+
 	if (log_verbosity > 50) {
 		cout << "Current h: " << h << endl;
 	}
@@ -187,7 +183,7 @@ void Simulate::SimulateOnce() {
 	size_t total_points = positions.size();
 	size_t free_points = total_points;
 
-	// env vars
+	// calc f_int
 	std::vector<vecf3> f_int(total_points, vecf3(0, 0, 0));
 
 
@@ -205,15 +201,8 @@ void Simulate::SimulateOnce() {
 		printf("Residual: %lf\n", calcResidual(vector<vecf3>(), x_vec, this->positions));
 	}
 
-	//VectorXf xf_vec(3 * free_points);
-	//for (size_t i = 0; i < free_points; i++) {
-	//	xf_vec(3 * i) = this->positions[xf_to_x[i]][0];
-	//	xf_vec(3 * i + 1) = this->positions[xf_to_x[i]][1];
-	//	xf_vec(3 * i + 2) = this->positions[xf_to_x[i]][2];
-	//}
 
-	//// the auxiliary vector b
-	//VectorXf b_vec = x_vec - K_mat.transpose() * K_mat * x_vec;
+
 
 
 	SparseMatrix<float> coeff_mat(3 * total_points, 3 * total_points);
@@ -223,7 +212,7 @@ void Simulate::SimulateOnce() {
 	// the k'th iteration 
 	auto positions_k = this->positions;
 
-	for (int iter_count = 0; iter_count < 50 ; iter_count++) {
+	for (int iter_count = 0; iter_count < 50; iter_count++) {
 
 
 		coeff_mat.setZero();
@@ -237,14 +226,12 @@ void Simulate::SimulateOnce() {
 
 		// initialize rhs_vec
 		for (int i = 0; i < total_points; i++) {
-			// notice!! the f_int(x) term and the (nabla_fint_comp * x_n) term is left empty, and to be filled
-			// during the iteration of the edgelist
-			
+
 
 			Vector3f x_n{ positions_k[i][0], positions_k[i][1], positions_k[i][2] };
 
 			// x_n from step k, but Y from step 0
-			rhs_vec.block(3 * i, 0, 3, 1) = (-calcG(i, x_n , Vector3f::Zero()))
+			rhs_vec.block(3 * i, 0, 3, 1) = (-calcG(i, x_n, Vector3f::Zero()))
 				+ Matrix3f::Identity() * mass[i] * x_n;
 		}
 
@@ -275,7 +262,7 @@ void Simulate::SimulateOnce() {
 
 			// here "-" * "-" = + (for h_squared_f) !!
 			rhs_vec.block(3 * vi, 0, 3, 1) += (h_squared_f)+(-h_squared_f_nabla * Vector3f{ positions_k[vi][0],positions_k[vi][1],positions_k[vi][2] });
-			rhs_vec.block(3 * vj, 0, 3, 1) += ( -h_squared_f)+(-h_squared_f_nabla * Vector3f{ positions_k[vj][0],positions_k[vj][1],positions_k[vj][2] });
+			rhs_vec.block(3 * vj, 0, 3, 1) += (-h_squared_f) + (-h_squared_f_nabla * Vector3f{ positions_k[vj][0],positions_k[vj][1],positions_k[vj][2] });
 		}
 
 		// solve the problem
@@ -309,7 +296,7 @@ void Simulate::SimulateOnce() {
 		}
 
 
-		// update pos
+		// update x
 		for (int i = 0; i < total_points; i++) {
 			positions_k[i][0] = x_vec(3 * i);
 			positions_k[i][1] = x_vec(3 * i + 1);
@@ -328,16 +315,116 @@ void Simulate::SimulateOnce() {
 		if (log_verbosity > 10) {
 			printf("[%d] Residual: %lf\n", iter_count, residual);
 		}
-		
+
 		if (fabs(residual) < 1e-3) {
 			break;
 		}
 	}
 
-	// update to positions & update velocity
+	// update v and x
 	for (int i = 0; i < positions.size(); i++) {
 		this->velocity[i] = ((positions_k[i] - positions[i]).cast_to<vecf3>() / h).cast_to<pointf3>();
 		this->positions[i] = positions_k[i];
 	}
 
+}
+
+void SetBlock(Eigen::SparseMatrix<float>& m, int si, int sj, int r, int c, Eigen::MatrixXf& b)
+{
+	for (int i = si; i < si + r; ++i) {
+		for (int j = sj; j < sj + c; ++j) {
+			m.coeffRef(i, j) += b(i - si, j - sj);
+		}
+	}
+}
+
+void Simulate::SimulateOnceNR() {
+
+
+	if (log_verbosity > 50) {
+		cout << "Current h: " << h << endl;
+	}
+
+	size_t total_points = positions.size();
+	size_t free_points = total_points;
+
+	// calc f
+	std::vector<vecf3> f_tot(total_points, vecf3(0, 0, 0));
+
+	for (size_t i = 0; i < this->f_ext.size(); ++i)f_tot[i] = f_ext[i];
+
+	std::vector<vecf3> f_int(total_points, vecf3(0, 0, 0));
+	for (int i = 0; i < total_points; ++i) {
+		auto adjv_list = getAdjV(i);
+		Vector3f tem(0, 0, 0);
+		for (auto adjv : adjv_list) {
+			tem += calcFint(i, adjv, this->positions);
+		}
+		for (int j = 0; j < 3; ++j)f_int[i][j] = tem[j];
+		f_tot[i] += f_int[i];
+	}
+
+	Eigen::VectorXf f(3 * total_points);
+	for (int i = 0; i < total_points; ++i) {
+		f(3 * i) = f_tot[i][0];
+		f(3 * i + 1) = f_tot[i][1];
+		f(3 * i + 2) = f_tot[i][2];
+	}
+
+	// calc M
+	Eigen::SparseMatrix<float> M(3 * total_points, 3 * total_points);
+	for (int i = 0; i < total_points; ++i) {
+		M.insert(3 * i, 3 * i) = this->mass[i];
+		M.insert(3 * i + 1, 3 * i + 1) = this->mass[i];
+		M.insert(3 * i + 2, 3 * i + 2) = this->mass[i];
+	}
+
+	//calc K
+	Eigen::SparseMatrix<float> K(3 * total_points, 3 * total_points);
+	for (int i = 0; i < total_points; ++i) {
+		auto adjv_list = getAdjV(i);
+		for (auto j : adjv_list) {
+			Eigen::MatrixXf Kii = calcNablaFintComponent(i, j, this->positions);
+			Eigen::MatrixXf Kij = -Kii;
+			SetBlock(K, 3 * i, 3 * i, 3, 3, Kii);
+			SetBlock(K, 3 * j, 3 * j, 3, 3, Kii);
+			SetBlock(K, 3 * i, 3 * j, 3, 3, Kij);
+			SetBlock(K, 3 * j, 3 * i, 3, 3, Kij);
+		}
+	}
+
+	//calc A
+	Eigen::SparseMatrix<float> A = M - h * h * K;
+
+	//calc b
+	Eigen::VectorXf vt(3 * total_points);
+	for (int i = 0; i < total_points; ++i) {
+		vt(3 * i) = this->velocity[i][0];
+		vt(3 * i + 1) = this->velocity[i][1];
+		vt(3 * i + 2) = this->velocity[i][2];
+	}
+	Eigen::VectorXf b = M * vt + h * f;
+
+	// calc v^(t + 1)
+	cout << "begin makeCompressed()" << endl;
+	A.makeCompressed();
+	SparseQR<SparseMatrix<float>, COLAMDOrdering<int>> solver;
+	solver.compute(A);
+	if (solver.info() != Eigen::Success) {
+		cout << "solver: decomposition was not successful." << endl;
+		return;
+	}
+	VectorXf nv = solver.solve(b);
+
+	// update v
+	for (int i = 0; i < total_points; ++i) {
+		for (int j = 0; j < 3; ++j)
+			this->velocity[i][j] = nv(3 * i + j);
+	}
+
+	// update x
+	for (int i = 0; i < total_points; ++i) {
+		for (int j = 0; j < 3; ++j)
+			this->positions[i][j] += this->velocity[i][j] * h;
+	}
 }
